4fc3153ba0657c20efacbbbde716a8f2db2f7413
Giorgi Lomidze <99074835+lomidze1@users.noreply.github.com>
2022-12-18 14:34:38 +0400

    Add files via upload

---
+++ grommet/src/js/components/Notification/index.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { LayerPositionType } from '../Layer';
+import { AnchorType } from '..';
+export type StatusType = 'critical' | 'warning' | 'normal' | 'info' | 'unknown';
+
+export interface NotificationProps {
+  actions?: AnchorType[];
+  global?: boolean;
+  title?: string;
+  message?: string | React.ReactNode;
+  status?: StatusType;
+  toast?:
+    | boolean
+    | {
+        autoClose?: boolean;
+        position?: LayerPositionType;
+      };
+  onClose?: (...args: any[]) => any;
+  icon?: JSX.Element;
+}
+
+declare const Notification: React.FC<NotificationProps>;
+
+export { Notification };
